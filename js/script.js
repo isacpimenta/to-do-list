24390f3cd973e105234773875aac9de5d5acf531
Isac Pimenta de Abreu <132321630+isacpimenta@users.noreply.github.com>
2024-08-01 00:45:16 -0300

    Finished Basic To-Do

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
+let oldInputValue;
 
 // FUNÇÕES
 const saveTodo = (text) => {
@@ -37,6 +38,26 @@ const saveTodo = (text) => {
     todoInput.focus();
 }
 
+const toggleForms = () => {
+    editForm.classList.toggle("hide")
+    todoForm.classList.toggle("hide")
+    todoList.classList.toggle("hide")
+}
+
+const updateTodo = (text) => {
+    const todos = document.querySelectorAll(".to-do")
+
+    todos.forEach((todo) => {
+
+        let todoTitle = todo.querySelector("h3")
+
+        if(todoTitle.innerText === oldInputValue){
+            todoTitle.innerText = text;
+        }
+
+    })
+}
+
 // EVENTOS
 todoForm.addEventListener("submit", (e) => {
 
@@ -52,6 +73,11 @@ todoForm.addEventListener("submit", (e) => {
 document.addEventListener("click", (e) => {
     const targetEl = e.target
     const parentEl = targetEl.closest("div");
+    let todoTitle;
+
+    if(parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText;
+    }
 
     if(targetEl.classList.contains("finish-to-do")){
         parentEl.classList.toggle("done")
@@ -65,6 +91,27 @@ document.addEventListener("click", (e) => {
     
     if(targetEl.classList.contains("edit-to-do")){
         console.log("Clicou para editar")
-        
+        toggleForms()
+
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
     }
+})
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    toggleForms()
+})
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const editInputValue = editInput.value
+
+    if(editInputValue) {
+        updateTodo(editInputValue) 
+    }
+
+    toggleForms()
 })
