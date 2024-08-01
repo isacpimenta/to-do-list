5354a8c59871c8e21d37bc09604bd60bfe5ec280
Isac Pimenta de Abreu <132321630+isacpimenta@users.noreply.github.com>
2024-08-01 00:05:32 -0300

    Finished one event

--- js/script.js
+++ js/script.js
@@ -8,17 +8,43 @@ const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 
 // FUNÇÕES
+const saveTodo = (text) => {
+    const todo = document.createElement("div");
+    todo.classList.add("to-do");
 
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
+    
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    todo.appendChild(editBtn);
+    
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    todo.appendChild(deleteBtn);
+
+    todoList.appendChild(todo);
+
+    todoInput.value = ""
+    todoInput.focus();
+}
 
 // EVENTOS
 todoForm.addEventListener("submit", (e) => {
 
-    e.preventDefault()
+    e.preventDefault();
 
-    const inputValue = todoInput.value
+    const inputValue = todoInput.value;
 
     if(inputValue) {
-        console.log(inputValue)
-        //save todo
-    }
-})
+        saveTodo(inputValue);
+    } 
+});
